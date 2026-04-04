de670d6d95c315a004e1cc2713e4e3874f7054f0
Ismail-0511 <ibragimov.ismail1105@gmail.com>
2026-04-04 19:49:10 +0900

    feat: develop login business logic

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -8,7 +8,7 @@ import { Member } from '../../libs/dto/member/member';
 export class MemberResolver {
     constructor(private readonly memberService: MemberService) {}
 
-    @Mutation(() => String)
+    @Mutation(() => Member)
     @UsePipes(ValidationPipe)
     public async signup(@Args('input') input: MemberInput): Promise<Member> {
         try{
@@ -22,12 +22,12 @@ export class MemberResolver {
         
     }
 
-    @Mutation(() => String)
+    @Mutation(() => Member)
      @UsePipes(ValidationPipe)
-    public async login(@Args('input') input: LoginInput): Promise<string> {
+    public async login(@Args('input') input: LoginInput): Promise<Member> {
         try{
         console.log('Mutation: login');
-        return this.memberService.login();
+        return this.memberService.login(input);
         } catch (err) {
             console.log("error, signup:", err);
             throw new InternalServerErrorException(err);
