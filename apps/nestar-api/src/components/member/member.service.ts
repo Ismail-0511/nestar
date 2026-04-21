aa44745a900fc5b20ff76919b0dd8da354b558c3
Ismail-0511 <ibragimov.ismail1105@gmail.com>
2026-04-22 06:15:12 +0900

    feat: create LikeTarget graphql api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -11,6 +11,9 @@ import { StatisticModifier, T } from '../../libs/types/common';
 import { ViewService } from '../view/view.service';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { Direction } from '../../libs/enums/common.enum';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
+import { LikeService } from '../like/like.service';
 
 @Injectable()
 export class MemberService {
@@ -18,6 +21,7 @@ export class MemberService {
     constructor(@InjectModel("Member") private readonly memberModel: Model<Member>,
 private authService: AuthService,
 private viewService: ViewService,
+private likeService: LikeService,
 
 ) {}
     
@@ -28,8 +32,8 @@ private viewService: ViewService,
             //TODO: Authentication via TOKEN
             result.accessToken = await this.authService.createToken(result)
             return result; 
-        } catch(err) {
-            console.log("Error, Service.model:", err)
+        } catch(err:any) {
+            console.log("Error, Service.model:", err.message)
             throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
         }
        
@@ -132,6 +136,33 @@ private viewService: ViewService,
   return result[0];
 }
 
+public async likeTargetMember(memberId: ObjectId, likeRefId: ObjectId): Promise<Member> {
+  // 1. Target (likelanayotgan) foydalanuvchi mavjudligini va aktivligini tekshirish
+  const target: Member = await this.memberModel
+    .findOne({ _id: likeRefId, memberStatus: MemberStatus.ACTIVE })
+    .exec();
+
+  if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+  const input: LikeInput = {
+    memberId: memberId,
+    likeRefId: likeRefId,
+    likeGroup: LikeGroup.MEMBER,
+  };
+
+  // 2. Like toggle mantig'i va statistikani yangilash
+  // Bu yerda modifier: 1 bo'lsa like qo'shiladi, -1 bo'lsa olinadi (toggle)
+  const modifier: number = await this.likeService.toggleLike(input); 
+  const result = await this.memberStatsEditor({ 
+    _id: likeRefId, 
+    targetKey: 'memberLikes', 
+    modifier: modifier 
+  });
+
+  if (!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+
+  return result;
+}
 
 
     public async getAllMembersByAdmin(input: MembersInquiry): Promise<Members> {
