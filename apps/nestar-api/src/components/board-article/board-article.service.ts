9bafa3c059473f106e37f2f7f81ca1bb444dc28e
Ismail-0511 <ibragimov.ismail1105@gmail.com>
2026-04-25 14:40:01 +0900

    feat: modify like interaction related logic

--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -9,7 +9,7 @@ import { Direction, Message } from '../../libs/enums/common.enum';
 import { BoardArticleStatus } from '../../libs/enums/board-article.enum';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { BoardArticleUpdate } from '../../libs/dto/board-article/board-article.update';
-import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { lookupAuthMemberLiked, lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
 import { StatisticModifier, T } from '../../libs/types/common';
 import { LikeService } from '../like/like.service';
 import { LikeInput } from '../../libs/dto/like/like.input';
@@ -113,6 +113,7 @@ public async getBoardArticles(memberId: ObjectId, input: BoardArticlesInquiry): 
           list: [
             { $skip: (input.page - 1) * input.limit },
             { $limit: input.limit },
+            lookupAuthMemberLiked(memberId),
             lookupMember, // Yozuvchi ma'lumotlarini qo'shish
             { $unwind: '$memberData' },
           ],
